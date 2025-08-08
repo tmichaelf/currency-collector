d27a67e69bd2bb9e5103bc547a359c4578e3e5d0
Michael Ferretti <fretts326@gmail.com>
2025-08-07 23:14:21 -0600

    UX: Flat currencies index; group denominations by type on show\n\n- Remove coins/paper split from index for simpler overview\n- Split show page into Coins and Paper sections using scopes

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,10 +1,14 @@
 class CurrenciesController < ApplicationController
   def index
+    # All active currencies
     @currencies = Currency.active.includes(:currency_denominations)
   end
 
   def show
     @currency = Currency.find_by(id: params[:id])
     @denominations = @currency.currency_denominations.active.includes(:currency_collections)
+
+    @coin_denominations = @denominations.where(denomination_type: 'coin')
+    @bill_denominations = @denominations.where(denomination_type: 'bill')
   end
 end
